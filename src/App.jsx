238b822fb375d5ad47695d646cd22c4cb2b99fd6
José Ramírez <jwh743@gmail.com>
2024-12-10 14:51:49 -0300

    Línea corregida e iconos corregidos

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Physics } from '@react-three/cannon'
 import { Ground } from './components/Ground'
 import { FPV as Fpv } from './components/FPV'
 import { Player } from './components/Player'
-import { Cubes } from './components/cubes'
+import { Cubes } from './components/Cubes'
 import { TextureSelector } from './components/TextureSelect'
 
 function App () {
